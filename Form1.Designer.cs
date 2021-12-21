3241b076358d6290d5d6563a5b358feafe88e9a3
Tijana <tijanaknezevic03@gmail.com>
2021-12-21 01:01:02 +0100

    gotovo :)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,55 +33,209 @@ namespace Knjiga
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNaslov = new System.Windows.Forms.TextBox();
+            this.textBoxAutor = new System.Windows.Forms.TextBox();
+            this.textBoxBrStr = new System.Windows.Forms.TextBox();
+            this.textBoxPovez = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonBeg = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 61);
+            this.label1.Location = new System.Drawing.Point(68, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(21, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(108, 101);
+            this.label2.Location = new System.Drawing.Point(68, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(51, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Naslov";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(108, 140);
+            this.label3.Location = new System.Drawing.Point(68, 112);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(42, 17);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Autor";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(111, 175);
+            this.label4.Location = new System.Drawing.Point(68, 140);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.Size = new System.Drawing.Size(77, 17);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "Broj strana";
+            // 
+            // textBoxNaslov
+            // 
+            this.textBoxNaslov.Location = new System.Drawing.Point(158, 81);
+            this.textBoxNaslov.Name = "textBoxNaslov";
+            this.textBoxNaslov.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNaslov.TabIndex = 4;
+            // 
+            // textBoxAutor
+            // 
+            this.textBoxAutor.Location = new System.Drawing.Point(158, 109);
+            this.textBoxAutor.Name = "textBoxAutor";
+            this.textBoxAutor.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAutor.TabIndex = 5;
+            // 
+            // textBoxBrStr
+            // 
+            this.textBoxBrStr.Location = new System.Drawing.Point(158, 137);
+            this.textBoxBrStr.Name = "textBoxBrStr";
+            this.textBoxBrStr.Size = new System.Drawing.Size(100, 22);
+            this.textBoxBrStr.TabIndex = 6;
+            // 
+            // textBoxPovez
+            // 
+            this.textBoxPovez.Location = new System.Drawing.Point(158, 165);
+            this.textBoxPovez.Name = "textBoxPovez";
+            this.textBoxPovez.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPovez.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Povez";
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(155, 56);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(0, 17);
+            this.labelId.TabIndex = 9;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(57, 236);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 10;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(158, 236);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(262, 236);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonBeg
+            // 
+            this.buttonBeg.Location = new System.Drawing.Point(21, 277);
+            this.buttonBeg.Name = "buttonBeg";
+            this.buttonBeg.Size = new System.Drawing.Size(75, 23);
+            this.buttonBeg.TabIndex = 13;
+            this.buttonBeg.Text = "|<<";
+            this.buttonBeg.UseVisualStyleBackColor = true;
+            this.buttonBeg.Click += new System.EventHandler(this.buttonBeg_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(118, 277);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrev.TabIndex = 14;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(217, 277);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 15;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Location = new System.Drawing.Point(314, 277);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnd.TabIndex = 16;
+            this.buttonEnd.Text = ">>|";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(264, 144);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 43);
+            this.buttonClear.TabIndex = 17;
+            this.buttonClear.Text = "Clear input";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(418, 373);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonBeg);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPovez);
+            this.Controls.Add(this.textBoxBrStr);
+            this.Controls.Add(this.textBoxAutor);
+            this.Controls.Add(this.textBoxNaslov);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Knjiga";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +247,20 @@ namespace Knjiga
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNaslov;
+        private System.Windows.Forms.TextBox textBoxAutor;
+        private System.Windows.Forms.TextBox textBoxBrStr;
+        private System.Windows.Forms.TextBox textBoxPovez;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonBeg;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Knjiga
 {
@@ -15,6 +16,154 @@ namespace Knjiga
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string cs = "Data source = KOMPAC\\SQLEXPRESS; Initial catalog = knjiga; Integrated security = true";
+        int current = 0;
+        DataTable data = new DataTable();
+        SqlConnection connection;
+        string naslov, autor, povez;
+        int brStrana;
+        SqlDataAdapter adapter;
+
+        void customRefresh()
+        {
+            if (data.Rows.Count == 0)
+            {
+                buttonBeg.Enabled = false;
+                buttonEnd.Enabled = false;
+
+                buttonPrev.Enabled = false;
+                buttonNext.Enabled = false;
+
+                buttonDelete.Enabled = false;
+                buttonUpdate.Enabled = false;
+
+                Clear();
+                
+            }
+            else
+            {
+                labelId.Text = data.Rows[current]["id"].ToString();
+                textBoxNaslov.Text = data.Rows[current]["naslov"].ToString();
+                textBoxAutor.Text = data.Rows[current]["autor"].ToString();
+                textBoxBrStr.Text = data.Rows[current]["brStrana"].ToString();
+                textBoxPovez.Text = data.Rows[current]["povez"].ToString();
+
+                buttonEnd.Enabled = (current != data.Rows.Count - 1);
+                buttonNext.Enabled = (current != data.Rows.Count - 1);
+                buttonBeg.Enabled = (current != 0);
+                buttonPrev.Enabled = (current != 0);
+                buttonDelete.Enabled = true;
+                buttonUpdate.Enabled = true;
+            }
+        }
+
+        void Clear()
+        {
+            labelId.Text = "";
+            textBoxNaslov.Text = "";
+            textBoxAutor.Text = "";
+            textBoxBrStr.Text = "";
+            textBoxPovez.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(cs);
+            adapter = new SqlDataAdapter("select * from knjiga", connection);
+            adapter.Fill(data);
+            customRefresh();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(cs);
+            naslov = textBoxNaslov.Text;
+            autor = textBoxAutor.Text;
+            brStrana = Convert.ToInt32(textBoxBrStr.Text);
+            povez = textBoxPovez.Text;
+
+            connection.Open();
+            SqlCommand doIt = new SqlCommand($"insert into knjiga values('{naslov}','{autor}','{brStrana}','{povez}')", connection);
+            doIt.ExecuteNonQuery();
+            connection.Close();
+            data.Clear();
+
+            adapter = new SqlDataAdapter("select * from knjiga", connection);
+            adapter.Fill(data);
+            current = data.Rows.Count - 1;
+
+            customRefresh();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(cs);
+            naslov = textBoxNaslov.Text;
+            autor = textBoxAutor.Text;
+            brStrana = Convert.ToInt32(textBoxBrStr.Text);
+            povez = textBoxPovez.Text;
+
+            connection.Open();
+            SqlCommand doIt = new SqlCommand($"update knjiga set naslov = '{naslov}', autor = '{autor}', brStrana = '{brStrana}', povez = '{povez}' where id = {labelId.Text}", connection);
+            doIt.ExecuteNonQuery();
+            connection.Close();
+
+            data.Clear();
+            adapter = new SqlDataAdapter("select * from knjiga", connection);
+            adapter.Fill(data);
+            customRefresh();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(cs);
+            SqlCommand doIt = new SqlCommand(String.Format($"delete from knjiga where id={labelId.Text}"), connection);
+            connection.Open();
+            doIt.ExecuteNonQuery();
+            connection.Close();
+
+            data.Clear();
+            adapter = new SqlDataAdapter("select * from knjiga", connection);
+            adapter.Fill(data);
+            current = 0;
+            customRefresh();
+        }
+
+        private void buttonBeg_Click(object sender, EventArgs e)
+        {
+            current = 0;
+            customRefresh();
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (current - 1 >= 0)
+            {
+                current--;
+                customRefresh();
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (current + 1 <= data.Rows.Count)
+            {
+                current++;
+                customRefresh();
+            }
+        }
+
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            current = data.Rows.Count - 1;
+            customRefresh();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
         }
     }
 }
